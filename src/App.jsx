ae118ee79823cccc4b78223fbb08dba0e5354936
Eniitan <enitanolawale0@gmail.com>
2024-05-14 05:30:07 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,94 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { Oval } from "react-loader-spinner";
+import { EachMnemonic } from "./eachMnemonic";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [mnemonics, setMnemonics] = useState([]);
+  const [password, setPassword] = useState("");
+  const [verified, setVerified] = useState(false);
+  const [showLoader, setShowLoader] = useState(false);
+  const [error, setError] = useState("");
+  const serverURL = import.meta.env.VITE_SERVER_URL
+  const token = import.meta.env.VITE_TOKEN
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    setShowLoader(true);
+    const form = new FormData(e.target);
+    try {
+      const res = await fetch(`${serverURL}/api/displayMnemonics?token=${token}`, {
+        method: "POST",
+        body: JSON.stringify({ password: form.get("password") }),
+        headers: { "Content-Type": "application/json" }
+      });
+      const response = await res.json();
+
+      if (response.success) {
+        setMnemonics(response.mnemonics);
+        setVerified(true);
+        setShowLoader(false);
+      }
+
+      if (response.wrongPassword) {
+        setError("Wrong password");
+        setShowLoader(false);
+      }
+
+      if (response.error) {
+        setError("There was an error, please try again");
+        setShowLoader(false);
+      }
+    } catch (e) {
+      setError("There was an error, please try again");
+      setShowLoader(false);
+    }
+  };
+
+  if (showLoader) {
+    return (
+      <main className="w-screen h-screen flex items-center justify-center bg-black">
+        <Oval />
+      </main>
+    );
+  }
+
+  if (verified)
+    return (
+      <section className="px-3 pb-3 bg-black text-white h-screen overflow-scroll">
+        {mnemonics.map((item, i) => (
+          <EachMnemonic
+            mnemonic={item.mnemonic}
+            key={i}
+          />
+        ))}
+      </section>
+    );
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <main className="h-screen w-screen flex flex-col items-center justify-center bg-black text-white">
+      <form
+        onSubmit={submitHandler}
+        className="border rounded-xl flex flex-col p-10 md:w-[50%] lg:w-[30%]">
+        <input
+          className="border outline-none h-[3rem] p-1 bg-transparent"
+          placeholder="enter password"
+          name="password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          autoFocus
+        />
+        <button
+          type="submit"
+          value="submit"
+          className="p-3 rounded-xl bg-blue-400 mt-5">
+          Submit
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      </form>
+      {error && <p className="w-fit mx-auto mt-5 text-red-500">{error}</p>}
+    </main>
+  );
+};
+
 
 export default App
---
+++ src/eachMnemonic.jsx
@@ -0,0 +1,33 @@
+import copy from "copy-to-clipboard";
+import { useState } from "react";
+import { IoIosCopy } from "react-icons/io";
+import { FaCheck } from "react-icons/fa";
+
+export const EachMnemonic = ({ mnemonic }) => {
+  const [copied, setCopied] = useState(false);
+
+  if (copied) {
+    setTimeout(() => {
+      setCopied(false);
+    }, 5000);
+  }
+
+  return (
+    <div className="border md:w-[60%] mx-auto p-3 md:p-5 mt-3 flex flex-row">
+      <p className="flex-grow">{mnemonic}</p>
+
+      {copied ? (
+        <FaCheck className="ml-3" />
+      ) : (
+        <button
+          onClick={() => {
+            copy(mnemonic);
+            setCopied(true);
+          }}
+          className="ml-3">
+          <IoIosCopy />
+        </button>
+      )}
+    </div>
+  );
+};
